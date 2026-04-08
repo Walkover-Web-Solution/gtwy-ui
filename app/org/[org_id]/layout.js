7d93addf8e065dded7b6c6f271af86534f696b6f
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2026-04-08 17:30:35 +0530

    refactor: proxy issue fixes

--- app/org/[org_id]/layout.js
+++ app/org/[org_id]/layout.js
@@ -7,7 +7,6 @@ import { getSingleMessage, switchOrg, switchUser } from "@/config/index";
 import { useCustomSelector } from "@/customHooks/customSelector";
 import { ThemeManager, useThemeManager } from "@/customHooks/useThemeManager";
 import { getAllApikeyAction } from "@/store/action/apiKeyAction";
-import { toast } from "react-toastify";
 import {
   createApiAction,
   createBridgeAction,
@@ -379,48 +378,6 @@ function layoutOrgPage({ children, params, searchParams, isEmbedUser, isFocus })
       document.head.appendChild(script);
     }
   }, [doctstar_embed_token, isEmbedUser]);
-
-  const PROXY_AUTH_TOKEN = getFromCookies("proxy_token");
-  // Initialize MSG91 proxy auth configuration
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Configuration for MSG91 Proxy Auth
-      const configuration = {
-        authToken: PROXY_AUTH_TOKEN,
-        pass: true,
-        type: "user-management",
-        exclude_role_ids: [process.env.NEXT_PUBLIC_PROXY_USER_ROLE_ID],
-        success: (data) => {
-          // get verified token in response
-          console.log("MSG91 Auth success response", data);
-          toast.success("Authentication verified successfully!");
-        },
-        failure: (error) => {
-          // handle error
-          console.log("MSG91 Auth failure reason", error);
-          toast.error("Authentication failed. Please try again.");
-        },
-      };
-
-      // Load MSG91 Proxy Auth Script
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "https://proxy.msg91.com/assets/proxy-auth/proxy-auth.js";
-      script.onload = function () {
-        if (typeof initVerification === "function") {
-          initVerification(configuration);
-        }
-      };
-      document.head.appendChild(script);
-
-      // Cleanup function to remove script on unmount
-      return () => {
-        if (script && script.parentNode) {
-          script.parentNode.removeChild(script);
-        }
-      };
-    }
-  }, []);
 
   useEffect(() => {
     if (isValidOrg) {
--- components/Withauth.js
+++ components/Withauth.js
@@ -99,6 +99,7 @@ const WithAuth = (Children) => {
         }
         const configuration = {
           referenceId: process.env.NEXT_PUBLIC_REFERENCEID,
+          type: "authorization",
           addInfo: {
             redirect_path: "/login",
           },
