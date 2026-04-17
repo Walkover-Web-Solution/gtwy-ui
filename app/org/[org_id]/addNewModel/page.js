e35012b36c045ed597c38bbb1ddf027906e1b290
yash raj soni <91207380+yashraj-exe@users.noreply.github.com>
2026-04-17 11:07:45 +0530

    Refactor page.js to remove unused searchParams
    Removed unused searchParams and related useEffect for modal opening.

--- app/org/[org_id]/addNewModel/page.js
+++ app/org/[org_id]/addNewModel/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useEffect, useState } from "react";
+import React, { use, useState } from "react";
 import { MODAL_TYPE } from "@/utils/enums";
 import { openModal } from "@/utils/utility";
 import AddNewModelModal from "@/components/modals/AddNewModal";
@@ -13,13 +13,11 @@ import { useDispatch } from "react-redux";
 import ModelUsageDetailsModal from "@/components/modals/ModelUsageDetailsModal";
 import DeleteModal from "@/components/UI/DeleteModal";
 import useDeleteOperation from "@/customHooks/useDeleteOperation";
-import { useSearchParams } from "next/navigation";
 
 export const runtime = "edge";
 
 const Page = ({ params }) => {
   const resolvedParams = use(params);
-  const searchParams = useSearchParams();
   const dispatch = useDispatch();
   const [usageDetailsData, setUsageDetailsData] = useState();
   const [selectedDataToDelete, setselectedDataToDelete] = useState();
@@ -103,11 +101,6 @@ const Page = ({ params }) => {
     );
   };
 
-  useEffect(() => {
-    if (searchParams?.get("openAddModel") === "true") {
-      openModal(MODAL_TYPE.ADD_NEW_MODEL_MODAL);
-    }
-  }, [searchParams]);
 
   return (
     <MainLayout>
