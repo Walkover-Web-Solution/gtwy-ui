9f5818e2c152074b5ba694f2f9dbd3c74af1700e
Yogesh Patel <yogeshpatel@gmail.com>
2026-04-22 15:22:26 +0530

    feat: auto connect tools after creation

--- components/gtwy_embed/ToolsConfiguration.js
+++ components/gtwy_embed/ToolsConfiguration.js
@@ -181,7 +181,7 @@ const ToolsConfiguration = ({
             <EmbedListSuggestionDropdownMenu
               params={params}
               searchParams={{}}
-              name="tools"
+              name={singleToolMode ? "preFunction" : "tool"}
               hideCreateFunction={false}
               onSelect={handleSelectFunction}
               onSelectPrebuiltTool={() => {}}
