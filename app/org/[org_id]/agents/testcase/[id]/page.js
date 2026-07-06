dd4742a386feedbd4c1fad860b87c8eadf1a618c
Yogesh Patel <yogeshpatel@gmail.com>
2026-07-06 15:00:11 +0530

    fix: embed fixed

--- app/org/[org_id]/agents/testcase/[id]/page.js
+++ app/org/[org_id]/agents/testcase/[id]/page.js
@@ -415,7 +415,12 @@ function TestCases({ params }) {
     const scriptId = "gtwy-user-script";
     if (document.getElementById(scriptId)) return;
 
-    const scriptURl = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/gtwy_embed_local.js`;
+    const scriptURl =
+      process.env.NEXT_PUBLIC_ENV === "LOCAL"
+        ? `${process.env.NEXT_PUBLIC_FRONTEND_URL}/gtwy_embed_local.js`
+        : process.env.NEXT_PUBLIC_ENV !== "PROD"
+          ? `${process.env.NEXT_PUBLIC_FRONTEND_URL}/gtwy_dev.js`
+          : `${process.env.NEXT_PUBLIC_FRONTEND_URL}/gtwy.js`;
     const script = document.createElement("script");
     script.id = scriptId;
     script.src = scriptURl;
