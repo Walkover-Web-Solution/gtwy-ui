0986f9ae503d242b28204483a87bbd227d4e199c
Yogesh Patel <yogeshpatel@gmail.com>
2026-07-25 13:13:08 +0530

    fix: minimax logo added

--- utils/utility.js
+++ utils/utility.js
@@ -17,6 +17,7 @@ import GeminiIcon from "@/icons/GeminiIcon";
 import GoogleDocIcon from "@/icons/GoogleDocIcon";
 import Grok from "@/icons/Grok";
 import GroqIcon from "@/icons/GroqIcon";
+import MinimaxIcon from "@/icons/minimax";
 import MistralIcon from "@/icons/MistralIcon";
 import MoonshotIcon from "@/icons/MoonshotIcon";
 import NeevCloudIcon from "@/icons/NeevCloudIcon";
@@ -312,6 +313,8 @@ export const getIconOfService = (service, height, width) => {
       return <MoonshotIcon height={height} width={width} />;
     case "neev_cloud":
       return <NeevCloudIcon height={height} width={width} />;
+    case "minimax":
+      return <MinimaxIcon height={height} width={width} />;
     default:
       return <OpenAiIcon height={height} width={width} />;
   }
