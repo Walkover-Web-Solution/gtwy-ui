111cc6ae0f64cb138aa19cea6633ff193981d9c1
Vishal Nema <vishalnema@viasocket.com>
2026-09-26 10:21:19 +0530

    fix: disable save until pre/post tool configuration changes (#2131)

--- components/configuration/configurationComponent/FunctionParameterModal.js
+++ components/configuration/configurationComponent/FunctionParameterModal.js
@@ -554,6 +554,7 @@ function FunctionParameterModal({
   functionName = "",
   variablesPath = {},
   setVariablesPath = () => {},
+  originalArgs = null,
   isMasterAgent = false,
   params = {},
   tool_name = "",
@@ -592,8 +593,7 @@ function FunctionParameterModal({
           const environment = function_details?.environment;
           setToolData({ ...function_details, thread_id, environment });
         } else {
-          const version_id = function_details?.version_id;
-          setToolData({ ...function_details, thread_id, version_id });
+          setToolData({ ...function_details, thread_id });
         }
       } else {
         setToolData({});
--- components/configuration/configurationComponent/PostEmbedList.js
+++ components/configuration/configurationComponent/PostEmbedList.js
@@ -176,6 +176,7 @@ const PostEmbedList = ({ params, searchParams, isPublished, isEditor = true, isE
           variablesPath={variablesPath}
           setVariablesPath={setVariablesPath}
           variables_path={variables_path}
+          originalArgs={post_tool_args}
         />
         <DeleteModal
           onConfirm={removePostFunction}
